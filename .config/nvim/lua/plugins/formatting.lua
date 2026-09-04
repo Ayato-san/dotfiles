f0fcb0c2967e8499f450017638cfadcd5eb270cd
Ayato-san <dev.github.collision@p.ayato-san.fr>
2026-09-04 10:04:01 +0200

    feat(nvim): configure prettier and eslint

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -2,10 +2,22 @@ return {
   {
     'stevearc/conform.nvim',
     opts = {
+      formatters_by_ft = {
+        astro = { 'prettier', 'oxfmt', stop_after_first = true },
+        javascript = { 'prettier', 'oxfmt', stop_after_first = true },
+        javascriptreact = { 'prettier', 'oxfmt', stop_after_first = true },
+        json = { 'prettier', 'oxfmt', stop_after_first = true },
+        jsonc = { 'prettier', 'oxfmt', stop_after_first = true },
+        svelte = { 'prettier', 'oxfmt', stop_after_first = true },
+        typescript = { 'prettier', 'oxfmt', stop_after_first = true },
+        typescriptreact = { 'prettier', 'oxfmt', stop_after_first = true },
+        vue = { 'prettier', 'oxfmt', stop_after_first = true },
+      },
+
       formatters = {
         oxfmt = {
-          prepend_args = function(self, ctx)
-            local project_config = vim.fs.find({
+          prepend_args = function(_, ctx)
+            local config = vim.fs.find({
               '.oxfmtrc.json',
               '.oxfmtrc.jsonc',
               'oxfmt.config.ts',
@@ -15,13 +27,12 @@ return {
               upward = true,
             })[1]
 
-            -- Config présente dans le projet :
-            -- on laisse oxfmt la détecter tout seul.
-            if project_config then
+            -- If a config file is found, use it;
+            if config then
               return {}
             end
 
-            -- Sinon, config personnelle globale.
+            -- If no config file is found, use the default config file in the user's home directory;
             return {
               '--config',
               vim.fn.expand('~/.config/oxfmt/.oxfmtrc.json'),
